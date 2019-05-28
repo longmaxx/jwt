9d49cee41def0ca751dbc22bb7617eaf9c1bcec9
longmaxx <longmaxx@yandex.ru>
2019-05-28 08:25:36 +0300

    make qsh, iat exp and correct jwt

--- JWTConsoleApp/Program.cs
+++ JWTConsoleApp/Program.cs
@@ -8,15 +8,24 @@ using JWT.Algorithms;
 using JWT.Serializers;
 using JWTConsoleApp.CommandLine.Utility;
 using System.Collections;
+using System.IO;
 
 namespace JWTConsoleApp
 {
     class Program
     {
+        String secret;
+
         static void Main(string[] args)
         {
             Arguments CommandLine = new Arguments(args);
-            string secret = "";
+            String secret = "";
+            String HttpMethod = "";
+            String BaseUrl = "";
+            String Url = "";
+            bool HasQshParam = false;
+            bool HasUrlParams = false;
+
             Dictionary<string, object> payload = new Dictionary<string, object>();
 
 
@@ -26,12 +35,15 @@ namespace JWTConsoleApp
             if (CommandLine.Parameters.Count == 0)
             {
                 Console.WriteLine("Command line:");
-                Console.WriteLine("/secretkey:\"\" /live:  <other payload parameters>");
+                Console.WriteLine("/secret:\"\" /live:  [/verb:\"\" /url:\"\" /baseurl:\"\"] [<other payload parameters>]");
                 Console.WriteLine("");
                 Console.WriteLine("Parameters are added to payload as is. But the tool has some supported parameters:");
-                Console.WriteLine("/secretkey:   - your secret key. This is required parameter.");
+                Console.WriteLine("/secret:   - your secret key. This is required parameter.");
                 Console.WriteLine("/accesskey:   - your access key. Added to payload as is.");
-                Console.WriteLine("/live:   - token live time in seconds. Generates claims 'iat, exp' in payload.");
+                Console.WriteLine("/live:   - token live time in milliseconds. Generates claims 'iat, exp' in payload.");
+                Console.WriteLine("/verb:   - Http method. Needed to generate QSH payload parameter. GET, POST, etc.");
+                Console.WriteLine("/baseurl:   - Base API url. Needed to generate QSH payload parameter.");
+                Console.WriteLine("/url:   - Full html query string. Needed to generate QSH payload parameter.");
                 Console.WriteLine("");
                 Console.WriteLine("example: /secretkey:\"flkjds02340ewjl\" /live:1000 /sub:\"vasuliy@mail.com\" /iss:\"KHDFSF32039kdfjlslk3l21jlk\"");
                 return;
@@ -39,34 +51,67 @@ namespace JWTConsoleApp
 
             foreach (DictionaryEntry de in CommandLine.Parameters)
             {
-                if (de.Key.ToString() == "secretkey")
+                String curKey = de.Key.ToString();
+                String curValue = de.Value.ToString();
+                if (curKey == "secret")
                 {
-                    secret = de.Value.ToString();
+                    secret = curValue;
                 }
-                else if (de.Key.ToString() == "accesskey")
+                else if (curKey == "accesskey")
                 {
-                    payload.Add("iss", de.Value.ToString());
+                    payload.Add("iss", curValue);
                 }
-                else if (de.Key.ToString() == "live")
+                else if (curKey == "live")
                 {
                     DateTimeOffset t_now = new DateTimeOffset(DateTime.Now);
-                    long nowSeconds = t_now.ToUnixTimeSeconds();
-                    payload.Add("iat", nowSeconds);
-                    payload.Add("exp", nowSeconds +  Convert.ToInt64(de.Value));
+                    long nowMSeconds = t_now.ToUnixTimeMilliseconds();
+                    payload.Add("iat", nowMSeconds);
+                    payload.Add("exp", nowMSeconds +  Convert.ToInt64(curValue));
+                }
+                else if (curKey == "verb")
+                {
+                    HttpMethod = curValue;
+                    HasUrlParams = true;
+                }
+                else if (curKey == "url")
+                {
+                    Url = curValue;
+                    HasUrlParams = true;
+                }
+                else if (curKey == "baseurl")
+                {
+                    BaseUrl = curValue;
+                    HasUrlParams = true;
+                }
+                else if (curKey == "qsh")
+                {
+                    HasQshParam = true;
+                    payload.Add(curKey, curValue);
                 }
                 else
                 {
-                    payload.Add(de.Key.ToString(),de.Value);
+                    payload.Add(curKey, curValue);
                 }
             }
-
+            if (HasUrlParams && HasQshParam)
+            {
+                TextWriter ErrWriter = Console.Error;
+                ErrWriter.WriteLine("Both '/qsh:' and url parameters (/verb:, /url:, /baseurl:) are detected. Please use only one way to set QSH payload parameter.");
+                Environment.Exit(2);
+            }
+            // Calculate QSH here
+            if (HttpMethod!="" && Url!="" && BaseUrl != "")
+            {
+                URICanonicalizer uc = new URICanonicalizer(HttpMethod, BaseUrl, Url);
+                payload["qsh"] = uc.QueryStringHash();
+            }
             IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
             IJsonSerializer serializer = new JsonNetSerializer();
             IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
             IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
-
             var token = encoder.Encode(payload, secret);
             Console.WriteLine(token);
+            //=====================================
         }
     }
 }
---
+++ JWTConsoleApp/URICanonicalizer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Security.Cryptography;
+namespace JWTConsoleApp
+{
+    class URICanonicalizer
+    {
+        protected   String Method;
+                    Uri Url;
+                    Uri BaseUrl;
+                    String CanonicalUrl;
+                    String CanonicalQuery;
+
+
+        public URICanonicalizer(String Method, String BaseUrl, String AbsoluteUrl)
+        {
+            this.Method = Method.ToUpper();
+            this.Url = new Uri(AbsoluteUrl);
+            this.BaseUrl = new Uri(BaseUrl);
+
+            CanonicalizeUrl();
+            CanonicalizeQuery();
+        }
+
+        public String MakeCanonicalUrl()
+        {
+            String sCanonical = Method;
+            if (CanonicalUrl != "")
+                sCanonical += "&" + CanonicalUrl;
+            if (CanonicalQuery != "")
+                sCanonical += "&" + CanonicalQuery;
+            return sCanonical;
+            //return Method + "&" + CanonicalUrl + "&" + CanonicalQuery;
+        }
+        public String QueryStringHash()
+        {
+            var crypt = new SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] cryptedBytes =  crypt.ComputeHash(Encoding.UTF8.GetBytes(MakeCanonicalUrl()));
+            foreach (byte theByte in cryptedBytes)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        protected void  CanonicalizeUrl()
+        {
+            String res = "";
+            // trim base url
+            String absPath = Url.AbsolutePath;
+            if (absPath.StartsWith(BaseUrl.AbsolutePath))
+                res = absPath.TrimStart(BaseUrl.AbsolutePath.ToCharArray());
+            //starts with /
+            if (!res.StartsWith("/"))
+                res = "/" + res;
+            if (res.Length >1 )
+            {
+                res.TrimEnd('/');
+            }
+            //no &  letter
+            //res = res.Replace("&", "%26");
+            res = HttpUtility.UrlPathEncode(res);
+            res = res.Replace("&", "%26");
+            // throw new Exception("Not implemented");
+            this.CanonicalUrl = res;
+        }
+
+        protected void CanonicalizeQuery()
+        {
+            String queryString = Url.Query.TrimStart('?');
+            if (queryString == "")
+            {
+                //this.CanonicalQuery = "";
+                return;
+            }
+                
+            List<String> lParams = queryString.Split('&').ToList();
+            Dictionary<String, String[]> dic = new Dictionary<String, String[]>(lParams.Count);
+            foreach(String sParam in lParams)
+            {
+                String[] paramArr = sParam.Split('=');
+                String paramName = paramArr[0];
+                String paramValue = paramArr[1];
+
+                String[] valArray;
+                if (!dic.Keys.Contains(paramName))
+                {
+                    valArray = new String[1];
+                    valArray[0] = paramValue;
+                    dic.Add(paramName, valArray);
+                }
+                else
+                {
+                    String[] ExistingValues;
+                    dic.TryGetValue(paramName, out ExistingValues);
+
+                    
+                    var newValArray = new String[ExistingValues.Length + 1];
+                    ExistingValues.CopyTo(newValArray, 0);
+                    newValArray[newValArray.Length - 1] = paramValue;
+                    dic[paramName] = newValArray.OrderBy(key => key).ToArray();
+                }
+            }
+            SortedDictionary<String, String[]> sDic = new SortedDictionary<String,String[]>(dic);
+            //ignore jwt
+            //urlencode keys
+            //urlencode values
+            //sort keys
+            //group same params by ,
+            sDic.Remove("jwt");
+            List<String> lOutQuery = new List<String>();
+            foreach(KeyValuePair<String, String[]> par in sDic)
+            {
+                String sOutQuery = "";
+                sOutQuery += HttpUtility.UrlPathEncode(par.Key);
+                sOutQuery += "=";
+                sOutQuery += HttpUtility.UrlPathEncode(String.Join(",", par.Value));
+                lOutQuery.Add(sOutQuery);
+            }
+            this.CanonicalQuery =  String.Join<String>("&", lOutQuery);
+        }
+    }
+}
